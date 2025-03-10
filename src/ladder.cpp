78915c64a5c9d98619577ec7e8a05d36e668c2f1
nreualuhs <lpshu@uci.edu>
2025-03-10 00:17:47 +0000

    figuring out edit distance

--- src/ladder.cpp
+++ src/ladder.cpp
@@ -19,7 +19,7 @@ bool edit_distance_within(const string& str1, const string& str2, int d) {
 }
 
 bool is_adjacent(const string& word1, const string& word2) {
-
+    return edit_distance_within(word1, word2, 1);
 }
 
 vector<string> generate_word_ladder(const string& begin_word, const string& end_word, const set<string>& word_list) {
@@ -46,7 +46,6 @@ vector<string> generate_word_ladder(const string& begin_word, const string& end_
                         return new_ladder;
                     }
                     ladder_queue.push(new_ladder);
-                    visited.insert(word);
                 }
             }
         }
---
+++ src/ladder_main.cpp
@@ -0,0 +1,8 @@
+#include <iostream>
+#include "ladder.h"
+
+using namespace std;
+
+int main() {
+    return 0;
+}
